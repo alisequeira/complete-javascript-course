9d2c84a005351c210bbfa8b288eca52808171ac1
Ali Jorgell Sequeira <alisequeira.la.240@gmail.com>
2020-07-21 15:31:12 -0400

    conditional statements if/else

---
+++ JS-basics/conditionals.js
@@ -0,0 +1,20 @@
+/*********************
+ * CONDITIONALS IF/ELSE STATEMENTS
+ */
+
+ var firstName = 'John';
+ var civilStatus = 'single';
+
+ if(civilStatus === 'married'){
+     console.log(firstName + ' is married!');
+ }else{
+     console.log(firstName + ' we hopefully marry soon :)');
+ }
+
+ var isMarried = true;
+
+ if(isMarried){
+    console.log(firstName + ' is married!');
+}else{
+    console.log(firstName + ' we hopefully marry soon :)');
+}
