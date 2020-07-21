b493f3bd9b806eb77228a630f6be8f259bd1a0ef
Ali Jorgell Sequeira <alisequeira.la.240@gmail.com>
2020-07-21 12:47:11 -0400

    variables and data types

---
+++ JS-basics/script.js
@@ -0,0 +1,25 @@
+console.log('hello word!!!!!');
+
+//variables en data type
+var firstName = 'john';//string
+console.log(firstName);
+
+var lastName = 'Smith';//string
+var age = 28;//number
+var fullAge = true;//boolean
+var bloodType;//undefined
+
+//mutation and type coercion
+console.log(firstName + ' ' + lastName);
+//declare many variables on the same line
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + 'is a ' + age + ' year old ' + job + ' ,is he married? ' + isMarried);
+
+//variable mutation
+age = 'twenty eight';
+job = ' driver';
+alert(firstName + 'is a ' + age + ' year old ' + job + ' ,is he married? ' + isMarried);
+
