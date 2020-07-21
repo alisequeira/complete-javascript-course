efd13324af7e60fc69f66a3fb6ec9ec07bf7f4cf
Ali Jorgell Sequeira <alisequeira.la.240@gmail.com>
2020-07-21 15:45:42 -0400

    boolean logical

--- JS-basics/conditionals.js
+++ JS-basics/conditionals.js
@@ -18,3 +18,20 @@
 }else{
     console.log(firstName + ' we hopefully marry soon :)');
 }
+
+/*************************
+ * BOOLEAN LOGIC
+ */
+
+ var firstName = 'john';
+ var age = 16;
+
+ if(age < 13){
+     console.log(firstName + ' is a boy.')
+ }else if(age >= 13 && age < 20){
+    console.log(firstName + ' is a teenager.')
+ }else if (age >= 20 && age < 30){
+    console.log(firstName + ' is a young man.')
+ }else{
+     console.log(firstName + ' is a man.');
+ }
