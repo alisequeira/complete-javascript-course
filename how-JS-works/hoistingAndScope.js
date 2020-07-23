9d1c239bed3fced0ab3cad1b48eb57b80291c116
Ali Jorgell Sequeira <alisequeira.la.240@gmail.com>
2020-07-23 11:34:45 -0400

    hoisting

---
+++ how-JS-works/hoistingAndScope.js
@@ -0,0 +1,26 @@
+/*************************8
+ * HOISTING
+ */
+calculateAge(1994);// it work
+
+ function calculateAge(year){
+     console.log(2020 - year);
+ }
+
+ //it won't work. hoisting is only executing in functions declaration
+
+ //retirement(1999);
+var retirement = function(year){
+    console.log(65 - (2020 - year));
+}
+console.log(age);
+var age = 23;
+
+function foo(){
+    var age = 65;
+    console.log(age);//prints out 65, cuz the execution context is from foo()
+}
+foo();
+console.log(age);//prints out 23, cuz the execution context is from the global object.
+
+
