846d8f17c08e17c56857c379a8bab33330ed8486
Ali Jorgell Sequeira <alisequeira.la.240@gmail.com>
2020-07-24 11:12:50 -0400

    objects, inheritance and prototype sumary

---
+++ advanced-JS/object.js
@@ -0,0 +1,16 @@
+/*****************************
+ * OBJECTS
+ */
+
+    //IN JS ALMOST EVERYTHING IS AN OBJECT
+
+    /*
+    1- Every JavaScript object has a prototype property, which makes de inheritance possible in JS
+    2- The property of an object is where we put methods and properties that we want other object to inherit.
+    3- The constructor's prototype property is NOT the prototype of the constructor itself, it's the prototype of all 
+    instances that are created through it.
+    4- When a certain method  (or property) is called, the search starts in the object itself, and if it cannot be found, the search
+    moves on to the object's prototype. This continues until the method is found: PROTOTYPE CHAIN.
+     */
+
+     
