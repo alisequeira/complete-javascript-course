0ab64814fffeb3a99ef89bb65b0bff7ec23cd6d3
Ali Jorgell Sequeira <alisequeira.la.240@gmail.com>
2020-07-27 12:59:06 -0400

    reading input data

--- budgety/app.js
+++ budgety/app.js
@@ -5,7 +5,26 @@ var budgetController = (function(){//module pattern is that it return an object 
 
 // UI CONTROLLER
 var UIController = (function(){
-    //some code
+    var DOMString = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+    return {
+        getInput: function (){
+            return {
+                type: document.querySelector(DOMString.inputType).value,//will be either inc or exp
+                description: document.querySelector(DOMString.inputDescription).value,
+                value: document.querySelector(DOMString.inputValue).value,
+            }
+            
+        },
+        getDOMStrings: function(){
+            return DOMString;
+        }
+
+    };
 })();
 
 //The two modules created by now are completely independent modules. So there will not be any interaction between these two
@@ -13,17 +32,20 @@ var UIController = (function(){
 // GLOBAL APP CONTROLLER
 var controller = (function( budgetCtrl, UICtrl){
 //modules can also recive arguments. we'll pass the ohter two modules as arguments, so this module knows about the other two and can connect them
+    var DOM = UICtrl.getDOMStrings();
 
     var CtrlAddItem = function (){
-           // 1. Get the filed input data.
+        // 1. Get the filed input data.
+
+            var input = UICtrl.getInput();
+            console.log(input);
         //2.  Add the item to the budget controller.
         //3. Add the item to the UI.
         //4. Calculate the budget
         //5. display the budget control to the UI.
-        console.log('hello word');
     }
 
-    document.querySelector('.add__btn').addEventListener('click', CtrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', CtrlAddItem);
 
     document.addEventListener('keypress', event =>{
         if(event.keyCode === 13 || event.which === 13){
