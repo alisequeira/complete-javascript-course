7bfcd2b2347ef79fa4eaf06d115cf01b2f0b7d8d
Ali Jorgell Sequeira <alisequeira.la.240@gmail.com>
2020-07-27 12:11:57 -0400

    setting up the fisrt event listeners

--- budgety/app.js
+++ budgety/app.js
@@ -1,32 +1,33 @@
+//  BUDGET CONTROLLER
 var budgetController = (function(){//module pattern is that it return an object containing all of the functions that we want to be public.
     
-    var x = 23;
-
-    var add = function(a){
-        return x + a;
-    }
-
-    return{
-        publicTest: function(b){
-            return add(b);
-        }
-    }
 })();
 
+// UI CONTROLLER
 var UIController = (function(){
     //some code
 })();
 
 //The two modules created by now are completely independent modules. So there will not be any interaction between these two
 
+// GLOBAL APP CONTROLLER
 var controller = (function( budgetCtrl, UICtrl){
 //modules can also recive arguments. we'll pass the ohter two modules as arguments, so this module knows about the other two and can connect them
 
-   var z =  budgetCtrl.publicTest(5);
-        return{
-            anotherPublicTest: function(){
-                console.log(z);
-            }
-        }
+    var CtrlAddItem = function (){
+           // 1. Get the filed input data.
+        //2.  Add the item to the budget controller.
+        //3. Add the item to the UI.
+        //4. Calculate the budget
+        //5. display the budget control to the UI.
+        console.log('hello word');
+    }
 
+    document.querySelector('.add__btn').addEventListener('click', CtrlAddItem);
+
+    document.addEventListener('keypress', event =>{
+        if(event.keyCode === 13 || event.which === 13){
+            CtrlAddItem();
+        }
+    });
 })(budgetController, UIController);
