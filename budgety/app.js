3da4568283885d1cf8df38c4fd586b34e9abe200
Ali Jorgell Sequeira <alisequeira.la.240@gmail.com>
2020-07-27 13:56:36 -0400

    working on expense and income function

--- budgety/app.js
+++ budgety/app.js
@@ -21,6 +21,34 @@ var budgetController = (function(){//module pattern is that it return an object 
             inc: 0
         }
     };
+
+    return {
+        addItem: function (type, des, val){
+            var newItem ,ID;
+            //create new ID
+            if(data.allItems[type].length > 0){
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            }else{
+                ID = 0;
+            }
+           
+
+            //create new item based on 'inc' or 'exp' type
+            if(type === 'exp'){
+                newItem= new Expense(ID, des, val);
+            }else if(type === 'inc'){
+                newItem = new Income(ID, des, val)
+            }
+           
+            //push it into our data structure
+            data.allItems[type].push(newItem);
+            //return the new element
+            return newItem;
+        },
+        testing: function (){
+            console.log(data);
+        }
+    };
     
 })();
 
@@ -67,11 +95,13 @@ var controller = (function( budgetCtrl, UICtrl){
 
 
     var CtrlAddItem = function (){
+        var input, newItem;
         // 1. Get the filed input data.
 
-            var input = UICtrl.getInput();
+             input = UICtrl.getInput();
             console.log(input);
         //2.  Add the item to the budget controller.
+             newItem =  budgetCtrl.addItem(input.type, input.description, input.value);
         //3. Add the item to the UI.
         //4. Calculate the budget
         //5. display the budget control to the UI.
