21e25268b9e015cd3146718a6d0e12fc850a6d81
Ali Jorgell Sequeira <alisequeira.la.240@gmail.com>
2020-07-29 13:16:14 -0400

    arrow function 2.0

--- ES6/arrowFunction.js
+++ ES6/arrowFunction.js
@@ -11,13 +11,13 @@
         console.log(ages5);
 
         //ES6
-            let ages6 = years.map(el => 2025 - el);
+            let ages6 = years.map(el => 2025 - el);//One argument no need parenthesis. One line of code no need return statement.
         console.log(ages6);
         
-            ages6 = years.map((el, index) => `Ages element ${index + 1}: ${2020 - el}.`);
+            ages6 = years.map((el, index) => `Ages element ${index + 1}: ${2020 - el}.`); // more than 1 argument use parenthesis
         console.log(ages6);
 
-            ages6 = years.map((el,index) => {
+            ages6 = years.map((el,index) => {//more than one line of code need curly bracket and return statemen.
                 const now = new Date().getFullYear();
                 const age = now - el;
               return  `Ages element ${index + 1}: ${age}.`;
